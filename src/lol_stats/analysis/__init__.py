1058198cc32f1c18bcebad15d24641c4eede4aff
Paul Fischer <paul_fischer94@hotmail.com>
2023-03-21 00:10:48 +0100

    feat: get fraction of games with smurfs

--- src/lol_stats/analysis/__init__.py
+++ src/lol_stats/analysis/__init__.py
@@ -1,6 +1,7 @@
 """Subpackage for game analysis."""
 
 from ._kda_distribution import plot_kda_distribution
+from ._smurf_detection import portion_games_with_smurfs_in_game
 from ._win_rate import (
     get_general_win_rate,
     get_win_rate_against_smurfs,
@@ -14,4 +15,5 @@ __all__ = [
     "get_win_rate_against_smurfs",
     "get_win_rate_with_smurfs",
     "get_win_rate_without_smurfs_in_game",
+    "portion_games_with_smurfs_in_game",
 ]
--- src/lol_stats/analysis/_smurf_detection.py
+++ src/lol_stats/analysis/_smurf_detection.py
@@ -2,7 +2,31 @@
 
 from lol_stats import data
 
+from . import _utils
+
 _MAX_SMURF_LEVEL = 60
+
+
+def portion_games_with_smurfs_in_game(games: data.Games) -> float:
+    """
+    Calculate the portion of games where smurfs are present.
+
+    Parameters
+    ----------
+    games
+        The games that should be analyzed.
+
+
+    Returns
+    -------
+    float
+        The portion.
+    """
+    counter = 0
+    for game in games.games:
+        if _utils.is_smurf_in_game(game):
+            counter += 1
+    return counter / len(games.games)
 
 
 def is_smurf(summoner: data.SummonerInGame) -> bool:
